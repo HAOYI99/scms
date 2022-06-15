6845f29548e5788fa50d78af0a6a9358d3160c86
HAOYI99 <84807215+HAOYI99@users.noreply.github.com>
2022-06-16 00:34:30 +0800

    Complete User Module

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:scms/models/user.dart';
 import 'package:scms/screens/profile/edit_password.dart';
 import 'package:scms/screens/profile/edit_profilepic.dart';
-import 'package:scms/screens/profile/editprofile.dart';
+import 'package:scms/screens/profile/edit_profile.dart';
 import 'package:scms/services/auth.dart';
 import 'package:scms/services/user_database.dart';
 import 'package:scms/shared/constants.dart';
@@ -16,7 +18,6 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
-  final _formKey = GlobalKey<FormState>();
   final AuthService _auth = AuthService();
   bool isLoading = false;
 
@@ -27,74 +28,256 @@ class _ProfileState extends State<Profile> {
     return isLoading
         ? loadingIndicator()
         : Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              title: const Text('Profile',
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0)),
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              actions: [
+                IconButton(
+                    onPressed: () async {
+                      setState(() => isLoading = true);
+                      await _auth.signOut().then((value) {
+                        showSuccessSnackBar('Signed Out', context);
+                      }).catchError((e) {
+                        showFailedSnackBar(e.toString(), context);
+                      });
+                    },
+                    icon: const Icon(
+                      Icons.logout_outlined,
+                      color: Colors.blue,
+                    ))
+              ],
+            ),
+            backgroundColor: Colors.white,
             body: StreamBuilder<UserData>(
                 stream: DatabaseService(uid: user!.uid).userData,
                 builder: (context, snapshot) {
                   if (snapshot.hasData) {
                     UserData? userData = snapshot.data;
-                    return Form(
-                      key: _formKey,
-                      child: SingleChildScrollView(
-                        child: Center(
-                          child: Column(
-                            children: <Widget>[
-                              const SizedBox(
-                                height: 200.0,
-                              ),
-                              GestureDetector(
-                                child: CircleAvatar(
-                                  backgroundColor: Colors.blue,
-                                  radius: 60.0,
-                                  child: CircleAvatar(
-                                    backgroundImage: userData!
-                                            .user_photo!.isNotEmpty
-                                        ? NetworkImage('${userData.user_photo}')
-                                        : AssetImage('assets/logo.png')
-                                            as ImageProvider,
-                                    radius: 55.0,
-                                    backgroundColor: Colors.blue,
-                                    child: Stack(
-                                      children: const [
-                                        Align(
-                                          alignment: Alignment.bottomRight,
-                                          child: CircleAvatar(
-                                            radius: 17.0,
-                                            child: Icon(Icons.edit),
-                                          ),
-                                        )
-                                      ],
-                                    ),
+                    return ListView(
+                      children: [
+                        Stack(
+                          children: [
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height -
+                                  (MediaQuery.of(context).padding.top +
+                                      kToolbarHeight),
+                              width: MediaQuery.of(context).size.width,
+                            ),
+                            //blueContainer
+                            Positioned(
+                              top: 50.0,
+                              child: Container(
+                                decoration: const BoxDecoration(
+                                  gradient: LinearGradient(
+                                      begin: Alignment.topRight,
+                                      end: Alignment.bottomLeft,
+                                      colors: [
+                                        Colors.indigoAccent,
+                                        Colors.lightBlue
+                                      ]),
+                                  borderRadius: BorderRadius.only(
+                                    topRight: Radius.circular(45),
                                   ),
                                 ),
-                                onTap: () {
-                                  _showEditPanel(userData);
-                                },
+                                height: MediaQuery.of(context).size.height,
+                                width: MediaQuery.of(context).size.width,
+                                child: Column(
+                                  children: [
+                                    buildNameTitle(context, userData),
+                                    buildGradientLine(),
+                                    buildProfileInfo(
+                                        context,
+                                        userData!.user_email!,
+                                        Icons.mail_sharp),
+                                    buildGradientLine(),
+                                    buildProfileInfo(
+                                        context,
+                                        userData.user_matricNo!,
+                                        MdiIcons.cardAccountDetailsOutline),
+                                    buildGradientLine(),
+                                    buildProfileInfo(
+                                        context,
+                                        userData.user_HPno!,
+                                        Icons.phone_android_outlined),
+                                    buildGradientLine(),
+                                    buildProfileInfo(
+                                        context,
+                                        userData.user_gender!,
+                                        genderIcon(userData.user_gender!)),
+                                    buildGradientLine(),
+                                    buildProfileInfo(
+                                        context,
+                                        userData.user_dob!,
+                                        Icons.date_range_outlined),
+                                    buildGradientLine(),
+                                    //address here
+                                    buildProfileInfo(
+                                        context,
+                                        getAddress(userData),
+                                        Icons.location_on_outlined),
+                                    buildGradientLine(),
+                                  ],
+                                ),
                               ),
-                              Text(userData.user_email!),
-                              Text(userData.user_lastName! +
-                                  ' ' +
-                                  userData.user_name!),
-                              ElevatedButton(
-                                  onPressed: () async {
-                                    setState(() => isLoading = true);
-                                    await _auth.signOut().then((value) {
-                                      showSuccessSnackBar(
-                                          'Signed Out', context);
-                                    }).catchError((e) {
-                                      showFailedSnackBar(e.toString(), context);
-                                    });
-                                  },
-                                  child: Text('Sign Out'))
-                            ],
-                          ),
-                        ),
-                      ),
+                            ),
+                            //avatar
+                            Positioned(
+                              top: 0.0,
+                              child: buildProfileAvatar(userData),
+                            ),
+                          ],
+                        )
+                      ],
                     );
                   } else {
                     return loadingIndicator();
                   }
                 }),
           );
+  }
+
+  IconData genderIcon(String gender) {
+    if (gender.isNotEmpty) {
+      if (gender == 'Male') {
+        return MdiIcons.genderMale;
+      } else {
+        return MdiIcons.genderFemale;
+      }
+    }
+    return MdiIcons.genderMaleFemale;
+  }
+
+  String getAddress(UserData userData) {
+    return userData.user_addStreet1! +
+        ', \n' +
+        userData.user_addStreet2! +
+        ', \n' +
+        userData.user_addPostcode! +
+        ' ' +
+        userData.user_addCity! +
+        ', \n' +
+        userData.user_addState! +
+        '.';
+  }
+
+  Container buildProfileInfo(
+      BuildContext context, String userData, IconData icon) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      child: Padding(
+          padding: const EdgeInsets.only(left: 30, top: 15, bottom: 15),
+          child: Row(children: [
+            Icon(icon, color: Colors.white, size: 25),
+            Padding(
+                padding: EdgeInsets.only(left: 25),
+                child: Text(userData,
+                    style: const TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white)))
+          ])),
+    );
+  }
+
+  Container buildRowProfileInfo(
+      BuildContext context, String userData, IconData icon) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.45,
+      child: Padding(
+          padding: const EdgeInsets.only(left: 30, top: 15, bottom: 15),
+          child: Row(children: [
+            Icon(icon, color: Colors.white, size: 25),
+            Padding(
+                padding: EdgeInsets.only(left: 25),
+                child: Text(userData,
+                    style: const TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white)))
+          ])),
+    );
+  }
+
+  Container buildRow2ProfileInfo(
+      BuildContext context, String userData, IconData icon) {
+    return Container(
+      decoration: const BoxDecoration(
+          border: Border(left: BorderSide(color: Colors.white, width: 1.5))),
+      child: Padding(
+          padding: const EdgeInsets.only(left: 30, top: 15, bottom: 15),
+          child: Row(children: [
+            Icon(icon, color: Colors.white, size: 25),
+            Padding(
+                padding: EdgeInsets.only(left: 25),
+                child: Text(userData,
+                    style: const TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white)))
+          ])),
+    );
+  }
+
+  DecoratedBox buildGradientLine() {
+    return DecoratedBox(
+      child: Container(
+        margin: const EdgeInsets.all(1),
+      ),
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(colors: [Colors.white, Colors.blue]),
+      ),
+    );
+  }
+
+  Container buildNameTitle(BuildContext context, UserData? userData) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      margin: EdgeInsets.only(left: 100),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 30, top: 25, bottom: 10),
+        child: Text(userData!.user_name! + ' ' + userData.user_lastName!,
+            style: const TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.white)),
+      ),
+    );
+  }
+
+  GestureDetector buildProfileAvatar(UserData? userData) {
+    return GestureDetector(
+      child: CircleAvatar(
+        backgroundColor: Colors.blue,
+        radius: 60.0,
+        child: CircleAvatar(
+          backgroundImage: userData!.user_photo!.isNotEmpty
+              ? NetworkImage('${userData.user_photo}')
+              : const AssetImage('assets/logo.png') as ImageProvider,
+          radius: 55.0,
+          backgroundColor: Colors.blue,
+          child: Stack(
+            children: const [
+              Align(
+                alignment: Alignment.bottomRight,
+                child: CircleAvatar(
+                  radius: 17.0,
+                  child: Icon(Icons.edit),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      onTap: () {
+        _showEditPanel(userData);
+      },
+    );
   }
 
   void _showEditPanel(UserData userData) {
